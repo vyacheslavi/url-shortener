5003e432f05c2184976252d3059d30b295e372a7
vyacheslavi <vyach.karepov@mail.ru>
2024-08-18 12:04:43 +0700

    add admin fields

--- url_shortener/api/admin.py
+++ url_shortener/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tokens
+
+
+@admin.register(Tokens)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = (
+        "id"
+        "full_url"
+        "short_url_token"
+        "owner"
+        "created_at"
+        "requests_count"
+        "is_active"
+    )
+    search_fields = ("full_url", "short_url_token")
+    ordering = ("-created_at",)
