1e87035e2c835e01f9503dade88adf18d352428e
vyacheslavi <vyach.karepov@mail.ru>
2024-08-20 13:34:21 +0700

    init admin panel

--- url_shortener/api/admin.py
+++ url_shortener/api/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 
-from .models import Tokens
+from .models import TokenURL
 
 
-@admin.register(Tokens)
+@admin.register(TokenURL)
 class TokenAdmin(admin.ModelAdmin):
     list_display = (
-        "id"
-        "full_url"
-        "short_url_token"
-        "owner"
-        "created_at"
-        "requests_count"
-        "is_active"
+        "full_url",
+        "short_url_token",
+        "owner",
+        "created_at",
+        "requests_count",
+        "is_active",
     )
     search_fields = ("full_url", "short_url_token")
     ordering = ("-created_at",)
