5524d1afbf61cc26b08b89a44a08d8b9bbe154cb
vyacheslavi <vyach.karepov@mail.ru>
2024-08-20 13:37:12 +0700

    add get_full_url and redirestion services

---
+++ url_shortener/api/services.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+from .models import TokenURL
+
+
+def get_full_url(token_url: str) -> str:
+    try:
+        token = TokenURL.objects.get(short_url_token__exact=token_url)
+        if not token.is_active:
+            raise KeyError("Token is not active")
+    except TokenURL.DoesNotExist:
+        raise KeyError("This urls does not exist in the database")
+    token.requests_count += 1
+    token.save()
+    return token.full_url
+
+
+def redirection(request, short_url):
+    try:
+        full_link = get_full_url(short_url)
+        return redirect(full_link)
+    except Exception as e:
+        return HttpResponse(e.args)
