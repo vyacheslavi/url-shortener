fe1709c4ed05cfc3ea213fa0c8c479a8b2f5d8fe
vyacheslavi <vyach.karepov@mail.ru>
2024-08-20 13:39:52 +0700

    add drf sessionid auth

--- url_shortener/url_shortener/urls.py
+++ url_shortener/url_shortener/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("api/v1/", include("api.urls")),
     path("<str:short_url>", services.redirection),
+    path("api/v1/auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
