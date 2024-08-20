935e556c835a873ce78758b50119f4cbd74d72f6
vyacheslavi <vyach.karepov@mail.ru>
2024-08-20 13:40:47 +0700

    add TokenURL model

--- url_shortener/api/models.py
+++ url_shortener/api/models.py
@@ -4,15 +4,17 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-from url_shortener.url_shortener import settings
-
-# Create your models here.
+from django.conf import settings
 
 
-class URL(models.Model):
-    id = models.PrimaryKeyField()
-    full_url = models.URLField(unique=True)
-    short_url = models.CharField(max_length=20, unique=True, db_index=True, blank=True)
+class TokenURL(models.Model):
+    full_url = models.URLField()
+    short_url_token = models.CharField(
+        max_length=settings.TOKEN_LENGTH,
+        unique=True,
+        db_index=True,
+        blank=True,
+    )
     owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -20,21 +22,26 @@ class URL(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     requests_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ("-created_at",)
+        unique_together = (
+            "full_url",
+            "owner",
+        )
 
     def save(self, *args, **kwargs):
-        if not self.short_url:
+        if not self.short_url_token:
             while True:
-                self.short_url = "".join(
+                self.short_url_token = "".join(
                     random.choices(
-                        settings.CHARACTERS,  # алфавит для генерации короткой ссылки мы будем хранить в файле настроек
-                        k=settings.TOKEN_LENGTH,  # длину короткой ссылки тоже
+                        settings.CHARACTERS,
+                        k=settings.TOKEN_LENGTH,
                     )
                 )
-                if not Tokens.objects.filter(  # проверка на уникальность
-                    short_url=self.short_url
+                if not TokenURL.objects.filter(
+                    short_url_token=self.short_url_token,
                 ).exists():
                     break
         super().save(*args, **kwargs)
