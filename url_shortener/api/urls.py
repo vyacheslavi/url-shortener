b8fb426693d73b42f7abc9735b0270a85e0687d3
vyacheslavi <vyach.karepov@mail.ru>
2024-08-23 11:18:55 +0700

    add to api router users path

--- url_shortener/api/urls.py
+++ url_shortener/api/urls.py
@@ -2,12 +2,13 @@ from django.urls import include, path
 from rest_framework import routers
 
 
-from .views import TokenURLViewSet
+from .views import TokenURLViewSet, UserTokenListAPIView
 
 app_name = "api"
 
 router = routers.DefaultRouter()
 router.register("tokens", TokenURLViewSet, basename="tokenurl")
+router.register("users", UserTokenListAPIView, basename="usertokens")
 
 urlpatterns = [
     path("", include(router.urls)),
